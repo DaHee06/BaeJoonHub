77ad88efd704d7c09558acfe14859ed6be2a0f9a
KIM DAHEE <103606851+DaHee06@users.noreply.github.com>
2024-03-12 21:08:01 +0900

    [Bronze V] Title: 대소문자 바꾸기, Time: 124 ms, Memory: 14200 KB -BaekjoonHub

---
+++ 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        char[] arr = str.toCharArray();
+        for (int i = 0; i < arr.length; i++) {
+            if (Character.isUpperCase(arr[i])) {
+                arr[i] = Character.toLowerCase(arr[i]);
+            } else {
+                arr[i] = Character.toUpperCase(arr[i]);
+            }
+        }
+        str = String.valueOf(arr);
+        System.out.println(str);
+    }
+}
