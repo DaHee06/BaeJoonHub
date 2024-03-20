f0b497d6c912b506e16ea4efb175107d00400019
KIM DAHEE <103606851+DaHee06@users.noreply.github.com>
2024-03-20 21:01:40 +0900

    [Silver V] Title: 회문인 수, Time: 152 ms, Memory: 16156 KB -BaekjoonHub

--- 백준/Silver/11068. 회문인 수/회문인 수.java
+++ 백준/Silver/11068. 회문인 수/회문인 수.java
@@ -1,27 +1,21 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
-/**
- * 11068. 회문인 수
- */
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        StringBuffer sb = new StringBuffer();
-        int T = Integer.parseInt(st.nextToken());
-        for(int i = 0; i< T ;i++){
-            st = new StringTokenizer(br.readLine());
-            int n = Integer.parseInt(st.nextToken());
+        int T = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0; i < T; i++) {
+            int n = Integer.parseInt(br.readLine());
             boolean found = false;
-            for(int B =2 ; B<65 ; B++){
+            for(int B = 2; B < 65; B++) {
                 String str = converse(n, B);
-
                 String str2 = new StringBuffer(str).reverse().toString();
-
                 if(str.equals(str2)) {
                     sb.append(1).append('\n');
                     found = true;
@@ -37,7 +31,7 @@ public class Main {
 
     //진법 변환
     public static String converse(int n, int B) {
-        StringBuffer sb = new StringBuffer();
+        StringBuilder sb = new StringBuilder();
 
         while(n > 0){
             sb.append((char)(n % B));
