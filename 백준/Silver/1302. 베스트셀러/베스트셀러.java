3f5b027482a3f67bafe107d0091cc30e96c804ee
KIM DAHEE <103606851+DaHee06@users.noreply.github.com>
2024-03-29 19:45:16 +0900

    [Silver IV] Title: 베스트셀러, Time: 128 ms, Memory: 14176 KB -BaekjoonHub

--- 백준/Silver/1302. 베스트셀러/베스트셀러.java
+++ 백준/Silver/1302. 베스트셀러/베스트셀러.java
@@ -1,41 +1,50 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.Map;
 
+/**
+ * 1. 가장 많이 팔린 책
+ * 2. 많이 팔린 책이 여러개 일 경우 사전 순 출력
+ */
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int N = Integer.parseInt(br.readLine());
 
-        HashMap<String, Integer> map = new HashMap<>();
-        String book = "";
-        int max = 0;
+        String[] arr = new String[N];
 
-        for(int i = 0 ; i <N ; i++){
-            String str = br.readLine();
-            if(map.containsKey(str)){
-                map.put(str, map.get(str)+1);
+        for(int i = 0 ; i< N ; i++){
+            arr[i] = br.readLine();
+        }
+
+        HashMap<String, Integer> map = new HashMap<>();
+
+        for(int i = 0 ;i < N ; i++){
+            if(map.containsKey(arr[i])){
+                map.put(arr[i], map.get(arr[i])+1);
             }else{
-                map.put(str, 1);
+                map.put(arr[i], 1);
             }
         }
+
+        int max = 0;
+        String str = "";
 
         for (String s : map.keySet()) {
-            if(map.get(s) > max){
-                book = s;
+            if(max < map.get(s)){
                 max = map.get(s);
-            }else if(map.get(s) == max && book.compareTo(s) > 0){
-                book = s;
-                max = map.get(s);
+                str = s;
+            }else if(max == map.get(s) && str.compareTo(s) > 0){
+                str = s;
             }
         }
-        bw.write(book);
 
-        br.close();
+        System.out.println(str);
 
-        bw.flush();
-        bw.close();
 
     }
 }
