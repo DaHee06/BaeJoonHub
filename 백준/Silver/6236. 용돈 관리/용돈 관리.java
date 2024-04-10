2a541f9ea17737401fe14e11aff027a37b156f1b
KIM DAHEE <103606851+DaHee06@users.noreply.github.com>
2024-04-10 21:44:49 +0900

    [Silver II] Title: 용돈 관리, Time: 248 ms, Memory: 21912 KB -BaekjoonHub

--- 백준/Silver/6236. 용돈 관리/용돈 관리.java
+++ 백준/Silver/6236. 용돈 관리/용돈 관리.java
@@ -8,43 +8,39 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int N = Integer.parseInt(st.nextToken()); // 사용 일수
-        int M = Integer.parseInt(st.nextToken()); // 인출 횟수
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
 
         int[] arr = new int[N];
         int max = 0;
-        for (int i = 0; i < N; i++) {
+        for(int i = 0 ; i < N ; i++){
             arr[i] = Integer.parseInt(br.readLine());
-            max = Math.max(max, arr[i]); // 사용할 금액 중 최댓값 찾기
+            max = Math.max(max , arr[i]);
         }
 
-        int lo = max; // 인출 금액의 최솟값은 사용할 금액의 최댓값
-        int hi = 100000 * N; // 인출 금액의 최댓값은 하루에 사용할 수 있는 금액의 최댓값
+        int lo = max ;
+        int hi = 10000 * N;
 
-        int result = hi; // 결과 변수 초기화
-        while (lo <= hi) {
-            int mid = (lo + hi) / 2; // 이분 탐색의 중간값 설정
-            int count = 1; // 인출 횟수 초기화
-            int sum = 0; // 현재 인출한 금액의 합 초기화
-
-            for (int i = 0; i < N; i++) {
-                // 현재까지 인출한 금액과 다음에 사용할 금액을 비교하여 인출 결정
-                if (sum + arr[i] > mid) {
-                    count++; // 새로운 인출 필요
-                    sum = 0; // 다음 인출에서 사용할 새로운 금액 합 초기화
+        int result = hi;
+        while(lo <= hi){
+            int K = (lo + hi) / 2;
+            int count = 1;
+            int pay = 0;
+            for(int i = 0; i < N ; i++){
+                if(pay + arr[i] > K){
+                    count++;
+                    pay = arr[i];
+                }else{
+                    pay += arr[i];
                 }
-                sum += arr[i]; // 다음에 사용할 금액 합 갱신
             }
 
-            // 인출 횟수가 M보다 작거나 같으면 결과 갱신 후 더 작은 값으로 이분 탐색
-            if (count <= M) {
-                result = Math.min(result, mid);
-                hi = mid - 1;
-            }
-            // 인출 횟수가 M보다 크면 인출 금액을 늘려야 하므로 범위를 늘림
+            if(count > M) lo = K + 1;
             else {
-                lo = mid + 1;
+                hi = K - 1;
+                result = Math.min(result, K);
             }
+
         }
 
         System.out.println(result);
