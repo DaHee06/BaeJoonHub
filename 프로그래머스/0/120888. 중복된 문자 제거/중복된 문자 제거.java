26328694d5b1c74485a3434bb4a3c6a3ffa942d1
KIM DAHEE <103606851+DaHee06@users.noreply.github.com>
2024-05-07 20:59:30 +0900

    [level 0] Title: 중복된 문자 제거, Time: 0.11 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.java
+++ 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.java
@@ -1,11 +1,19 @@
+import java.util.HashSet;
+import java.util.Set;
+
 class Solution {
     public String solution(String my_string) {
-        String answer = "";
-        for(int i = 0;i<my_string.length(); i++){
-            if(my_string.indexOf(my_string.charAt(i)) == i){
-                answer += my_string.charAt(i);
-             }
+
+                StringBuilder sb = new StringBuilder();
+
+        Set<Character> set = new HashSet<>();
+
+        for(int i = 0 ; i < my_string.length();i++){
+            if(!set.contains(my_string.charAt(i))){
+                set.add(my_string.charAt(i));
+                sb.append(my_string.charAt(i));
+            }
         }
-        return answer;
+        return sb.toString();
     }
 }
